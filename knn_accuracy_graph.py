61dc0378bcd12b5470d2677a78f06d883f652438
Oz <Shizuha7@yahoo.co.jp>
2018-04-06 07:41:03 +0900

    最近傍点数に対する訓練制度とテスト精度の比較

---
+++ knn_accuracy_graph.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+
+training_accuracy = []  # 訓練精度
+test_accuracy = []      # テスト精度
+neighbors_settings = range(1, 11)
+
+for n_neighbors in neighbors_settings:
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+
+    training_accuracy.append(clf.score(X_train, y_train))
+    test_accuracy.append(clf.score(X_test, y_test))
+
+plt.plot(neighbors_settings, training_accuracy, label='training accuracy')
+plt.plot(neighbors_settings, test_accuracy, label='test accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('n_neighbors')
+plt.legend()
+plt.show()
